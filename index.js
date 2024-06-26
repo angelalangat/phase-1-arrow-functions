cbda09fabf6ead4e8f6a684ee3f789d676f07f57
angie <angelaclangat@gmai.com>
2024-06-26 20:18:02 +0300

    Adds index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// Define the variables
+let x = 2;
+let a = 3;
+let b = 4;
+
+// Arrow function for dividing 2000 by 100
+const divide = () => 2000 / 100;
+
+// Arrow function for squaring a number
+const square = x => x * x;
+
+// Arrow function for adding two numbers
+const add = (a, b) => a + b;
+
